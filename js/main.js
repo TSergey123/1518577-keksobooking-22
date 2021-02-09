0333eb1a1c9684ffabb0f05fb38c5e8dce818659
toporkov-sv <toporkov123561@mail.ru>
2021-02-09 18:11:47 +0300

    Не понял задание

--- js/main.js
+++ js/main.js
@@ -22,3 +22,38 @@ function getRandomFloat(min, max, commas) {
 
 getRandomFloat(1.1, 1.2, 1);
 getRandomInteger(1, 100);
+
+function getRandomArrayElement(elements){
+  return elements[getRandomInteger(0, elements.length - 1)];
+}
+
+let author = {
+  avatar: 'img/avatars/user0' + getRandomInteger(1, 8) + '.png',
+}
+
+// let rooms = getRandomInteger(1, 999);
+let offer = {
+  title: 'qwe',
+  address: '{{location.x}} {{location.y}',
+  price: '123',
+  type: 'palace',
+  rooms: 10,
+  guests: 10,
+  checkin: '13:00',
+  checkout: '12:00',
+  features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+  description: 'qqq',
+  photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
+}
+
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+const createPhoto = () => {
+  return{
+    photo: getRandomArrayElement(PHOTOS),
+  };
+};
+
+// console.log(createPhoto);
+// console.log(author);
+// console.log(getRandomArrayElement(PHOTOS));
+
