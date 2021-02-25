d2b0d0a3f7ac7fc483ac99ce21577c7f5819710b
toporkov-sv <toporkov123561@mail.ru>
2021-02-25 18:29:51 +0300

    Добавил комментарии

--- js/popup.js
+++ js/popup.js
@@ -4,6 +4,7 @@ const similarPub = createPublication();
 const block = document.querySelector('.block');
 const canvas = document.querySelector('#map-canvas');
 block.innerHTML = '<img class="popup__avatar" width="70" height="70" alt="Аватар пользователя" src="' + similarPub.author.avatar + '"></img>';
+// Block - добавил просто проверки. Почему-то внутри него аватарка отображается верно а в блоке map-canvas - нет ;(
 
 // if(similarPub.offer.rooms > 1 && similarPub.offer.guests > 1){
 //   block.innerHTML = similarPub.offer.rooms +' комнаты для ' + similarPub.offer.guests + ' гостей';
@@ -17,6 +18,7 @@ block.innerHTML = '<img class="popup__avatar" width="70" height="70" alt="Ава
 // if (similarPub.offer.rooms > 1 && similarPub.offer.rooms < 2){
 //   block.innerHTML = similarPub.offer.rooms +' комнаты для ' + similarPub.offer.guests + ' гостя';
 // }
+//Хотел спросить, что вообще делать в таких случаях?
 
 
 
@@ -34,6 +36,7 @@ cardElement.querySelector('.popup__title').textContent = similarPub.offer.title;
 cardElement.querySelector('.popup__text--address').textContent = similarPub.offer.address;
 cardElement.querySelector('.popup__text--price').textContent = similarPub.offer.price + '₽/ночь';
 cardElement.querySelector('.popup__type').textContent = getRuType[similarPub.offer.type];
+//Обьясни пожалуйста, как это вообще сработало? почему они не сплюсовались, а сравнились?
 cardElement.querySelector('.popup__text--capacity').textContent = similarPub.offer.rooms +' комнаты для ' + similarPub.offer.guests + ' гостей';
 cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + similarPub.offer.checkin + ' выезд до ' + similarPub.offer.checkout;
 cardElement.querySelector('.popup__features').innerHTML = '<li class="popup__feature popup__feature--' + similarPub.offer.features + '">' + similarPub.offer.features + '</li>';
@@ -45,7 +48,8 @@ cardElement.querySelector('.popup__description').textContent = similarPub.offer.
 //   const photoElement = '<img class="popup__photo" width="45" height="40" alt="Фотография жилья" src="' + photoList + '"></img>';
 //   photoGallery.insertAdjacentHTML('beforeend', photoElement);
 // }
+//Вроде как надо было сделать лист, видимо что бы отображалось несколько фотографий одновременно, но как сделать не понял. Вроде сделал что-то похожее на то что было в лекции, но не работает вообще
+
 cardElement.querySelector('.popup__photos').innerHTML = '<img class="popup__photo" width="45" height="40" alt="Фотография жилья" src="' + similarPub.offer.photos + '"></img>';
 cardElement.querySelector('.popup__avatar').innerHTML = '<img class="popup__avatar" width="70" height="70" alt="Аватар пользователя" src="' + similarPub.author.avatar + '"></img>';
-cardElement.querySelector('.popup__title').textContent = similarPub.offer.title;
 canvas.append(cardElement);
