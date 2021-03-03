3bd8e6b56209aa8136dc2eee065146de5df3896a
toporkov-sv <toporkov123561@mail.ru>
2021-03-03 14:17:42 +0300

    Все еще не понимаю

--- js/form.js
+++ js/form.js
@@ -1,5 +1,8 @@
 const type = document.querySelector('#type');
 const price = document.querySelector('#price');
+const timeIn = document.querySelector('#timein');
+const timeOut = document.querySelector('#timeout');
+
 
 const typePrice = {
   bungalow: '0',
@@ -8,21 +11,19 @@ const typePrice = {
   palace: '10000',
 }
 
-// if (type.textContent = bungalow){
-//   price.innerHTML = 0;
-// }
+const chooseTime = (evt) => {
+  timeIn.value = evt.target.value,
+  timeOut.value = evt.target.value
+}
+timeOut.addEventListener('click', chooseTime);
+timeIn.addEventListener('click', chooseTime);
+
+// как сделать так что бы например при выборе первого элемента из первого списка
+// во втором списке бы выбирался например второй элемент из списка?
+// понимаю что это не нужно, но просто, для общего понимания
 
 const typeChange = () => {
   price.placeholder = typePrice[type.value];
-  // price.value = typePrice[type.value];
+  price.min = typePrice[type.value];
 }
-
-typeChange();
-
-// Изменения работают, но только при перезугрузки страницы. Например если в html
-// изменить selected, то плейсхолдер меняется, но при изменениях на странице, Ничего
-// не происходит ;( подскажи пожалуйста
-
-
-
-
+type.addEventListener('click', typeChange);
--- js/popup.js
+++ js/popup.js
@@ -4,8 +4,7 @@ const similarPub = createPublication();
 const block = document.querySelector('.block');
 const canvas = document.querySelector('#map-canvas');
 block.innerHTML = '<img class="popup__avatar" width="70" height="70" alt="Аватар пользователя" src="' + similarPub.author.avatar + '"></img>';
-// Block - добавил просто проверки. Почему-то внутри него аватарка отображается верно а в блоке map-canvas - нет ;(
-
+// Все еще не понял, почему в блоке вне шаблона, аватарки каждый раз новые, а в шаблоне всегда одинаковая
 // if(similarPub.offer.rooms > 1 && similarPub.offer.guests > 1){
 //   block.innerHTML = similarPub.offer.rooms +' комнаты для ' + similarPub.offer.guests + ' гостей';
 // }
@@ -19,8 +18,6 @@ block.innerHTML = '<img class="popup__avatar" width="70" height="70" alt="Ава
 //   block.innerHTML = similarPub.offer.rooms +' комнаты для ' + similarPub.offer.guests + ' гостя';
 // }
 //Хотел спросить, что вообще делать в таких случаях?
-
-
 
 const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
 const cardElement = cardTemplate.cloneNode(true);
@@ -36,19 +33,56 @@ cardElement.querySelector('.popup__title').textContent = similarPub.offer.title;
 cardElement.querySelector('.popup__text--address').textContent = similarPub.offer.address;
 cardElement.querySelector('.popup__text--price').textContent = similarPub.offer.price + '₽/ночь';
 cardElement.querySelector('.popup__type').textContent = getRuType[similarPub.offer.type];
-//Обьясни пожалуйста, как это вообще сработало? почему они не сплюсовались, а сравнились?
 cardElement.querySelector('.popup__text--capacity').textContent = similarPub.offer.rooms +' комнаты для ' + similarPub.offer.guests + ' гостей';
 cardElement.querySelector('.popup__text--time').textContent = 'Заезд после ' + similarPub.offer.checkin + ' выезд до ' + similarPub.offer.checkout;
 cardElement.querySelector('.popup__features').innerHTML = '<li class="popup__feature popup__feature--' + similarPub.offer.features + '">' + similarPub.offer.features + '</li>';
-cardElement.querySelector('.popup__description').textContent = similarPub.offer.title;
+cardElement.querySelector('.popup__description').textContent = similarPub.offer.description;
 
-// for (let photoList of similarPub.offer.photos){
-//   const photoGallery = cardElement.querySelector('.popup__photos');
-//   photoGallery.innerHTML = '';
-//   const photoElement = '<img class="popup__photo" width="45" height="40" alt="Фотография жилья" src="' + photoList + '"></img>';
-//   photoGallery.insertAdjacentHTML('beforeend', photoElement);
+//Первый вариант - Получаем бесконечное множество пустых фотографий.
+// const photosGallery = cardElement.querySelector('.popup__photos');
+// photosGallery.innerHTML = '';
+// const photo = '<img class="popup__photo" width="45" height="40" alt="Фотография жилья" src="' + similarPub.offer.photos + '"></img> ';
+// for (let photosList of similarPub.offer.photos) {
+//   const element = `<img src="${photosList}" class="popup__photo" width="50" height="40" alt="Изображение места"></img>`;
+//   photosGallery.insertAdjacentHTML('beforeend', element);
 // }
-//Вроде как надо было сделать лист, видимо что бы отображалось несколько фотографий одновременно, но как сделать не понял. Вроде сделал что-то похожее на то что было в лекции, но не работает вообще
+
+// Второй вариант - Получаем бесконечное множество одинаковых фотографий.
+// const photosGallery = cardElement.querySelector('.popup__photos');
+// photosGallery.innerHTML = '';
+// const photo = '<img class="popup__photo" width="45" height="40" alt="Фотография жилья" src="' + similarPub.offer.photos + '"></img> ';
+// for (let photosList of similarPub.offer.photos) {
+//   const element = `<img src="${similarPub.offer.photos}" class="popup__photo" width="50" height="40" alt="Изображение места"></img>`;
+//   photosGallery.insertAdjacentHTML('beforeend', element);
+// }
+
+// Третий вариант
+// const photosGallery = cardElement.querySelector('.popup__photos');
+// photosGallery.innerHTML = '';
+// const photo = '<img class="popup__photo" width="45" height="40" alt="Фотография жилья" src="' + similarPub.offer.photos + '"></img> ';
+// for (let photosList of similarPub.offer.photos) {
+//   const element = ` <img src="${similarPub.offer.photos}" class="popup__photo" width="50" height="40" alt="Изображение места"></img> `;
+//   // photosGallery.appendChild(element); //- Пустота
+//   // photosGallery.append(element); //- Бесконечный текст
+// }
+
+// Четвертый вариант
+// const fragment = document.createDocumentFragment();
+// const photoGallery = cardElement.querySelector('.popup__photos');
+// const photoTemplate = photoGallery.querySelector('.popup__photo');
+// fragment.appendChild(similarPub.offer.photos);
+
+
+// photoGallery.innerHTML = '';
+// for (let photoList of similarPub.offer.photos){
+//   photoList = photoTemplate.cloneNode(true);
+//   const photoElement = '<img class="popup__photo" width="45" height="40" alt="Фотография жилья" src="' + similarPub.offer.photos + '"></img>';
+//   fragment.appendChild(photoElement);
+//   // photoGallery.innerHTML = '';
+//   // photoGallery.append(fragment);
+//   // photoGallery.insertAdjacentHTML('beforeend', photoElement);
+//   // photoGallery.innerHTML = photoElement;
+// }
 
 cardElement.querySelector('.popup__photos').innerHTML = '<img class="popup__photo" width="45" height="40" alt="Фотография жилья" src="' + similarPub.offer.photos + '"></img>';
 cardElement.querySelector('.popup__avatar').innerHTML = '<img class="popup__avatar" width="70" height="70" alt="Аватар пользователя" src="' + similarPub.author.avatar + '"></img>';
